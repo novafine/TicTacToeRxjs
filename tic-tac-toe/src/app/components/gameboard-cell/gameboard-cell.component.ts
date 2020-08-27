88790a39e5a7f1bd772e6c2a38249704819e2267
nova <nova@lironfine.com>
2020-08-27 09:21:22 +0300

    feat(types): Added types file

--- tic-tac-toe/src/app/components/gameboard-cell/gameboard-cell.component.ts
+++ tic-tac-toe/src/app/components/gameboard-cell/gameboard-cell.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Player } from "../../types.type";
 
 @Component({
   selector: 'gameboard-cell',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gameboard-cell.component.scss']
 })
 export class GameboardCellComponent implements OnInit {
+
+  @Input() player: Player;
 
   constructor() { }
 
